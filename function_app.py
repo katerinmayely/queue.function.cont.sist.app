841ef7b6c91b90821c1dc390bd7fd12fdec4d457
Kattherine Hernandez <katerinmayely@gmail.com>
2024-11-27 21:29:06 -0600

    first commit

---
+++ function_app.py
@@ -0,0 +1,13 @@
+import azure.functions as func
+import datetime
+import json
+import logging
+
+app = func.FunctionApp()
+
+
+@app.queue_trigger(arg_name="azqueue", queue_name="queue-storage-requests",
+                               connection="QueueAzureWebJobsStorage") 
+def QueueTriggerFunctionActivateAccount(azqueue: func.QueueMessage):
+    body = azqueue.get_body().decode('utf-8')
+    logging.info( f"QueueTriggerFunctionActivateAccount: {body}" )
